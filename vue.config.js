4e087fa8259bcec7e12dfa5bd3903f0b509e3f1f
Romain <texier.r@gmail.com>
2022-04-08 14:06:48 +0200

    @upd publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV !== 'production' ? '/green-your-look-vue/' : '/',
+  publicPath: process.env.NODE_ENV !== 'production' ? '/' : '/green-your-look-vue/',
   transpileDependencies: [
     'vuetify'
   ]
