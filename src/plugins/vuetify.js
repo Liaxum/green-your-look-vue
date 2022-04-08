56842c40f94dfad468ad3350fe0620f6054b5878
Romain <texier.r@gmail.com>
2022-04-08 15:41:11 +0200

    @upd change color

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,17 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+	theme: {
+		themes: {
+			light: {
+				primary: "#6B2737",
+				secondary: "#F8F0DF",
+				accent: "#79B4B7",
+				info: "#777777",
+			},
+		},
+	},
 });
