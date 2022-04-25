714fabb3cf73812c3b4035bd0b9f6674880f8ee5
Romain <texier.r@gmail.com>
2022-04-25 18:01:32 +0200

    @upd url server

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import axios from "axios";
 import vuetify from "./plugins/vuetify";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "http://localhost:8542";
+axios.defaults.baseURL = "http://78.228.62.74:8542";
 
 Vue.config.productionTip = false;
 
