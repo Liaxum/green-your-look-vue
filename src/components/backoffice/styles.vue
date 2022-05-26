f715f082eb24e127dfac6534be69a06965553de9
melaniedrevet <melanie.univ@gmail.com>
2022-05-26 11:00:44 +0200

    update backoffice name

--- src/components/backoffice/styles.vue
+++ src/components/backoffice/styles.vue
@@ -1,52 +1,47 @@
 <template>
-    <v-card flat>
-        <v-card-text>
-            <v-data-table fixed-header :headers="headers" :items="items">
-                <template #item.action>
-                    <v-btn fab icon color="orange">
-                        <v-icon>
-                            mdi-pencil
-                        </v-icon>
-                    </v-btn>
-                    <v-btn icon flab color="red">
-                        <v-icon>
-                            mdi-delete
-                        </v-icon>
-                    </v-btn>
-                </template>
-            </v-data-table>
-        </v-card-text>
-    </v-card>
+  <v-card flat>
+    <v-card-text>
+      <v-data-table fixed-header :headers="headers" :items="items">
+        <template #item.action>
+          <v-btn fab icon color="orange">
+            <v-icon> mdi-pencil </v-icon>
+          </v-btn>
+          <v-btn icon flab color="red">
+            <v-icon> mdi-delete </v-icon>
+          </v-btn>
+        </template>
+      </v-data-table>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
 export default {
-    name: 'Categories',
-    data() {
-        return {
-            items: [
-                {
-                    id: 0,
-                    name: 'Test'
-                }
-            ],
-            headers: [
-                {
-                    text: 'Nom',
-                    value: 'name'
-                },
-                {
-                    text: '',
-                    value: 'action',
-                    sortable: false,
-                    width: '20%'
-                }
-            ]
-        }
-    }
-}
+  name: "Styles",
+  data() {
+    return {
+      items: [
+        {
+          id: 0,
+          name: "Test",
+        },
+      ],
+      headers: [
+        {
+          text: "Nom",
+          value: "name",
+        },
+        {
+          text: "",
+          value: "action",
+          sortable: false,
+          width: "20%",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
