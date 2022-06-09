0ebabdaae69241e0f00b0a201656b4935d5c4969
melaniedrevet <melanie.univ@gmail.com>
2022-06-09 14:25:12 +0200

    add blog / identite

--- src/views/StaticViews/Blog.vue
+++ src/views/StaticViews/Blog.vue
@@ -1,41 +1,109 @@
 <template>
-  <v-container class="my-5">
-    <h1 style="text-align: center; margin-bottom:30px;">Bienvenue sur notre Blog !</h1>
-    <v-row class="my-5" align="center">
+  <v-container class="mb-5">
+    <h1>
+      Bienvenue sur notre Blog !
+    </h1>
+    <v-row align="center">
       <v-col cols="12" md="4" sm="12">
         <v-card width="400px">
-          <v-img max-width="400px" src="@/assets/blog/look-ete.png" alt="look-ete"></v-img>
-          <v-card-title style="justify-content:center;">Femmes - Top 10 des looks été</v-card-title>
+          <v-img
+            max-width="400px"
+            src="@/assets/blog/look-ete.png"
+            alt="look-ete"
+          ></v-img>
+          <v-card-title
+            style="
+              justify-content: center;
+              font-family: 'Josefin Sans', sans-serif;
+              font-weight: 400;
+            "
+            >Femmes - Top 10 des looks été</v-card-title
+          >
         </v-card>
       </v-col>
       <v-col cols="12" md="4" sm="12">
         <v-card width="400px">
-          <v-img max-width="400px" src="@/assets/blog/conseils.png" alt="conseils"></v-img>
-          <v-card-title style="justify-content:center;">Bien s'habiller : conseils et astuces</v-card-title>
+          <v-img
+            max-width="400px"
+            src="@/assets/blog/conseils.png"
+            alt="conseils"
+          ></v-img>
+          <v-card-title
+            style="
+              justify-content: center;
+              font-family: 'Josefin Sans', sans-serif;
+              font-weight: 400;
+            "
+            >Bien s'habiller : conseils et astuces</v-card-title
+          >
         </v-card>
       </v-col>
       <v-col cols="12" md="4" sm="12">
         <v-card width="400px">
-          <v-img max-width="400px" src="@/assets/blog/ecolo.png" alt="ecolo"></v-img>
-          <v-card-title style="justify-content:center;">Acheter écolo à petit prix !</v-card-title>
+          <v-img
+            max-width="400px"
+            src="@/assets/blog/ecolo.png"
+            alt="ecolo"
+          ></v-img>
+          <v-card-title
+            style="
+              justify-content: center;
+              font-family: 'Josefin Sans', sans-serif;
+              font-weight: 400;
+            "
+            >Acheter écolo à petit prix !</v-card-title
+          >
         </v-card>
       </v-col>
       <v-col cols="12" md="4" sm="12">
         <v-card width="400px">
-          <v-img max-width="400px" src="@/assets/blog/homme-automne.png" alt="homme-automne"></v-img>
-          <v-card-title style="justify-content:center;">Hommes - Top 3 des looks automne</v-card-title>
+          <v-img
+            max-width="400px"
+            src="@/assets/blog/matieres.png"
+            alt="homme-automne"
+          ></v-img>
+          <v-card-title
+            style="
+              justify-content: center;
+              font-family: 'Josefin Sans', sans-serif;
+              font-weight: 400;
+            "
+            >3 matières éco-responsables
+          </v-card-title>
         </v-card>
       </v-col>
       <v-col cols="12" md="4" sm="12">
         <v-card width="400px">
-          <v-img max-width="400px" src="@/assets/blog/femme-automne.png" alt="femme-automne"></v-img>
-          <v-card-title style="justify-content:center;">Femmes - Top 3 des looks automne</v-card-title>
+          <v-img
+            max-width="400px"
+            src="@/assets/blog/femme-automne.png"
+            alt="femme-automne"
+          ></v-img>
+          <v-card-title
+            style="
+              justify-content: center;
+              font-family: 'Josefin Sans', sans-serif;
+              font-weight: 400;
+            "
+            >Femmes - Top 3 des looks automne</v-card-title
+          >
         </v-card>
       </v-col>
       <v-col cols="12" md="4" sm="12">
         <v-card width="400px">
-          <v-img max-width="400px" src="@/assets/blog/ethique.png" alt="ethique"></v-img>
-          <v-card-title style="justify-content:center;">La mode éthique expliquée</v-card-title>
+          <v-img
+            max-width="400px"
+            src="@/assets/blog/ethique.png"
+            alt="ethique"
+          ></v-img>
+          <v-card-title
+            style="
+              justify-content: center;
+              font-family: 'Josefin Sans', sans-serif;
+              font-weight: 400;
+            "
+            >La mode éthique expliquée</v-card-title
+          >
         </v-card>
       </v-col>
     </v-row>
@@ -48,5 +116,12 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+h1 {
+  font-family: "Josefin Sans", sans-serif;
+  font-weight: 500;
+  font-size: 30px;
+  text-align: center; 
+  margin: 40px 0;
+}
 </style>
--- src/views/StaticViews/Identite.vue
+++ src/views/StaticViews/Identite.vue
@@ -1,13 +1,171 @@
 <template>
-
+  <v-container class="mb-5">
+    <h1 style="text-align: center; margin-bottom: 30px">
+      Découvrez les valeurs de Green Your Look
+    </h1>
+    <v-row align="center" justify="center" style="margin: 50px 0">
+      <v-col cols="4" md="1" sm="2" class="center">
+        <v-img max-width="120" src="../../assets/icons/vegan.png"></v-img>
+      </v-col>
+      <v-col cols="4" md="1" sm="2" class="center">
+        <v-img max-width="120" src="../../assets/icons/recycle.png"></v-img>
+      </v-col>
+      <v-col cols="4" md="1" sm="2" class="center">
+        <v-img max-width="120" src="../../assets/icons/bio.png"></v-img>
+      </v-col>
+      <v-col cols="4" md="1" sm="2" class="center">
+        <v-img max-width="120" src="../../assets/icons/europe.png"></v-img>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="6" sm="12" class="center">
+        <v-card class="my-5">
+          <v-card-title
+            style="
+              justify-content: center;
+              font-family: 'Josefin Sans', sans-serif;
+              font-weight: 500;
+              color: #6b2737;
+            "
+            >Vegan</v-card-title
+          >
+          <v-card-text
+            style="
+              justify-content: center;
+              font-family: 'Josefin Sans', sans-serif;
+              font-weight: 400;
+            "
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Mattis
+            rhoncus urna neque viverra. Ut sem nulla pharetra diam. Fermentum
+            odio eu feugiat pretium nibh ipsum consequat nisl. Id venenatis a
+            condimentum vitae sapien pellentesque habitant morbi tristique.
+            Vitae semper quis lectus nulla at. Id ornare arcu odio ut sem nulla
+            pharetra diam sit. Pharetra diam sit amet nisl. Senectus et netus et
+            malesuada fames. Urna duis convallis convallis tellus id interdum
+            velit laoreet. Orci phasellus egestas tellus rutrum. A condimentum
+            vitae sapien pellentesque habitant. Nec sagittis aliquam malesuada
+            bibendum.
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="6" sm="12" class="center">
+        <v-card class="my-5">
+          <v-card-title
+            style="
+              justify-content: center;
+              font-family: 'Josefin Sans', sans-serif;
+              font-weight: 500;
+              color: #6b2737;
+            "
+            >Matières recyclées</v-card-title
+          >
+          <v-card-text
+            style="
+              justify-content: center;
+              font-family: 'Josefin Sans', sans-serif;
+              font-weight: 400;
+            "
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium
+            aenean pharetra magna ac placerat vestibulum. Justo donec enim diam
+            vulputate ut pharetra sit amet. Condimentum id venenatis a
+            condimentum vitae. Commodo quis imperdiet massa tincidunt. Ultrices
+            sagittis orci a scelerisque purus semper. Facilisi etiam dignissim
+            diam quis enim lobortis. Dui ut ornare lectus sit. Convallis
+            convallis tellus id interdum velit. Amet nisl purus in mollis.
+            Ultricies lacus sed turpis tincidunt id. Risus nullam eget felis
+            eget nunc lobortis mattis aliquam faucibus. Lacus vestibulum sed
+            arcu non. Eu feugiat pretium nibh ipsum. Eget nullam non nisi est
+            sit amet.
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="6" sm="12" class="center">
+        <v-card class="my-5">
+          <v-card-title
+            style="
+              justify-content: center;
+              font-family: 'Josefin Sans', sans-serif;
+              font-weight: 500;
+              color: #6b2737;
+            "
+            >Bio</v-card-title
+          >
+          <v-card-text
+            style="
+              justify-content: center;
+              font-family: 'Josefin Sans', sans-serif;
+              font-weight: 400;
+            "
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla
+            facilisi morbi tempus iaculis urna id volutpat lacus. Etiam erat
+            velit scelerisque in. Suspendisse potenti nullam ac tortor vitae
+            purus faucibus ornare. Neque ornare aenean euismod elementum nisi
+            quis eleifend quam. Tincidunt dui ut ornare lectus. Id diam vel quam
+            elementum pulvinar etiam. Urna nunc id cursus metus aliquam
+            eleifend. Pulvinar pellentesque habitant morbi tristique senectus et
+            netus et. Est ante in nibh mauris cursus mattis molestie. Tellus in
+            metus vulputate eu scelerisque felis imperdiet proin. Tellus rutrum
+            tellus pellentesque eu tincidunt tortor aliquam nulla facilisi.
+            Vitae et leo duis ut diam quam nulla. Vitae turpis massa sed
+            elementum tempus egestas. Risus sed vulputate odio ut enim. Tellus
+            mauris a diam maecenas.</v-card-text
+          >
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="6" sm="12" class="center">
+        <v-card class="my-5">
+          <v-card-title
+            style="
+              justify-content: center;
+              font-family: 'Josefin Sans', sans-serif;
+              font-weight: 500;
+              color: #6b2737;
+            "
+            >Fabrication européenne</v-card-title
+          >
+          <v-card-text
+            style="
+              justify-content: center;
+              font-family: 'Josefin Sans', sans-serif;
+              font-weight: 400;
+            "
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacinia
+            quis vel eros donec ac odio tempor orci. Mauris ultrices eros in
+            cursus turpis massa tincidunt dui. Morbi quis commodo odio aenean
+            sed adipiscing diam donec adipiscing. Tellus rutrum tellus
+            pellentesque eu. Elit at imperdiet dui accumsan sit amet. Aliquet
+            nibh praesent tristique magna. Lectus urna duis convallis convallis
+            tellus. Ipsum dolor sit amet consectetur adipiscing elit duis. Urna
+            neque viverra justo nec ultrices dui sapien. Ut tristique et egestas
+            quis ipsum suspendisse ultrices. Semper risus in hendrerit gravida
+            rutrum quisque non tellus. Et magnis dis parturient montes nascetur
+            ridiculus mus mauris. Laoreet suspendisse interdum consectetur
+            libero id faucibus nisl tincidunt eget.</v-card-text
+          >
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name: "Identite",
-}
+  name: "Identite",
+};
 </script>
 
-<style>
-
+<style scoped>
+h1 {
+  font-family: "Josefin Sans", sans-serif;
+  font-weight: 500;
+  font-size: 30px;
+  text-align: center;
+  margin: 40px 0;
+}
 </style>
