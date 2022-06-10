87942039d85ac40cdd85d5442ad0dcfdd2a1a3b8
melaniedrevet <melanie.univ@gmail.com>
2022-06-10 09:51:29 +0200

    add items

--- src/components/cards/ItemCard.vue
+++ src/components/cards/ItemCard.vue
@@ -1,78 +1,73 @@
 <template>
-	<v-expansion-panels>
-		<v-expansion-panel>
-			<v-expansion-panel-header>
-				Blouse courte à nouer
-			</v-expansion-panel-header>
-			<v-expansion-panel-content>
-				<v-card elevation="0">
-					<v-row>
-						<v-col cols="12" sm="4">
-							<v-img
-								max-width="150px"
-								:src="require(`@/assets/${img}`)"
-								alt="look"
-							></v-img>
-						</v-col>
-						<v-col cols="12" sm="8">
-							<v-card-text>
-								<div>
-									Labels :
-									Bio
-								</div>
-								<div>
-									Prix :
-									21,40€
-								</div>
-							</v-card-text>
-							<v-card-text>
-								<v-chip-group
-									v-model="
-										selection
-									"
-									active-class="primary accent-4 white--text"
-									column
-								>
-									<v-chip
-										>XS</v-chip
-									>
-									<v-chip
-										>S</v-chip
-									>
-									<v-chip
-										>M</v-chip
-									>
-									<v-chip
-										>L</v-chip
-									>
-									<v-chip
-										>XL</v-chip
-									>
-									<v-chip
-										>XXL</v-chip
-									>
-								</v-chip-group>
-							</v-card-text>
-						</v-col>
-					</v-row>
-				</v-card>
-			</v-expansion-panel-content>
-		</v-expansion-panel>
-	</v-expansion-panels>
+  <v-expansion-panels>
+    <v-expansion-panel class="mb-2" v-for="(item, i) in look.items" :key="i">
+      <v-expansion-panel-header>
+        {{ item.name }}
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <v-card elevation="0">
+          <v-row>
+            <v-col cols="12" sm="4">
+              <v-img
+                :src="require(`@/assets/femmes/${item.img}.png`)"
+                alt="look"
+              ></v-img>
+            </v-col>
+            <v-col cols="12" sm="8">
+              <v-card-text
+                style="
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                "
+              >
+                <div v-for="(label, i) in look.label" :key="i">
+                  <v-img
+                    class="mr-5"
+                    :src="require('@/assets/icons/' + label + '.png')"
+                    alt="bio"
+                    max-width="30"
+                  ></v-img>
+                </div>
+                <div style="font-size: 18px; margin-left: auto">
+                  {{ item.price }}
+                </div>
+              </v-card-text>
+              <v-card-text>
+                <v-chip-group
+                  v-model="selection"
+                  active-class="primary accent-4 white--text"
+                  column
+                >
+                  <v-chip>XS</v-chip>
+                  <v-chip>S</v-chip>
+                  <v-chip>M</v-chip>
+                  <v-chip>L</v-chip>
+                  <v-chip>XL</v-chip>
+                  <v-chip>XXL</v-chip>
+                </v-chip-group>
+              </v-card-text>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script>
-	export default {
-		name: "ItemCard",
-		data() {
-			return {
-				selection: 1,
-			};
-		},
-		props: {
-			img: { required: true, type: String },
-		},
-	};
+export default {
+  name: "ItemCard",
+  data() {
+    return {
+      selection: 1,
+    };
+  },
+  props: {
+    img: { required: true, type: String },
+    look: { required: true, type: Object },
+  },
+};
 </script>
 
 <style></style>
--- src/components/cards/LookCard.vue
+++ src/components/cards/LookCard.vue
@@ -1,255 +1,342 @@
 <template>
-	<v-container>
-		<v-row align="center">
-			<v-col
-				cols="12"
-				md="3"
-				sm="6"
-				xs="12"
-				v-for="(look, l) in looks"
-				:key="l"
-			>
-				<v-card
-					max-width="220"
-					@click.stop="dialog = true"
-				>
-					<v-img
-						alt="look"
-						:src="
-							require(`@/assets/${img}`)
-						"
-					></v-img>
-					<v-card-title
-						style="
-							font-family: 'Josefin Sans',
-								sans-serif;
-							font-weight: 400;
-						"
-					>
-						{{ look.name }}
-						<v-spacer></v-spacer>
-						<span>{{ look.price }}</span>
-					</v-card-title>
-					<v-divider></v-divider>
-					<v-item-group
-						style="
-							display: flex;
-							justify-content: center;
-						"
-					>
-						<v-item
-							v-for="(
-								lab, i
-							) in look.label"
-							:key="i"
-							class="mx-2 my-4"
-						>
-							<v-img
-								:src="
-									require('@/assets/icons/' +
-										lab +
-										'.png')
-								"
-								alt="bio"
-								max-width="30"
-							></v-img>
-						</v-item>
-					</v-item-group>
-				</v-card>
-				<v-dialog
-					v-model="dialog"
-					max-width="1000"
-					hide-overlay
-					:retain-focus="false"
-					persistent
-				>
-					<v-card
-						max-width="1000"
-						style="
-							font-family: 'Josefin Sans',
-								sans-serif;
-							font-weight: 400;
-						"
-					>
-						<v-card-actions>
-							<v-card-title>
-								Look
-							</v-card-title>
-							<v-spacer></v-spacer>
-							<v-btn
-								color="primary"
-								icon
-								fab
-								@click="
-									dialog = false
-								"
-							>
-								<v-icon
-									>mdi-close</v-icon
-								>
-							</v-btn>
-						</v-card-actions>
-						<v-row>
-							<v-col
-								cols="12"
-								md="4"
-								sm="12"
-								xs="12"
-							>
-								<v-img
-									style="
-										margin: 0
-											30px
-											30px
-											30px;
-									"
-									max-width="400px"
-									:src="
-										require(`@/assets/${img}`)
-									"
-									alt="look"
-								></v-img>
-							</v-col>
-							<v-col
-								cols="12"
-								md="7"
-								sm="12"
-								xs="12"
-							>
-								<item-card :img="img"/>
-								<v-divider></v-divider>
-								<v-card-text>
-									Article
-									2
-								</v-card-text>
-								<v-divider></v-divider>
-								<v-card-text>
-									Article
-									3
-								</v-card-text>
-								<v-divider></v-divider>
-							</v-col>
-						</v-row>
-						<v-card-actions>
-							<v-spacer></v-spacer>
-							<v-btn
-								style="
-									margin: 20px
-										10px
-										20px
-										0;
-								"
-								large
-								outlined
-								color="primary"
-							>
-								Ajouter aux
-								favoris
-							</v-btn>
-							<v-btn
-								style="
-									margin: 20px
-										10px
-										20px
-										0;
-								"
-								large
-								outlined
-								color="primary"
-							>
-								Ajouter au
-								panier
-							</v-btn>
-						</v-card-actions>
-					</v-card>
-				</v-dialog>
-			</v-col>
-		</v-row>
-	</v-container>
+  <v-container>
+    <v-row align="center">
+      <v-col
+        cols="12"
+        md="3"
+        sm="6"
+        xs="12"
+        v-for="(look, l) in looks"
+        :key="l"
+      >
+        <v-card max-width="220" @click.stop="dialog = true">
+          <v-img alt="look" :src="require(`@/assets/${img}`)"></v-img>
+          <v-card-title
+            style="font-family: 'Josefin Sans', sans-serif; font-weight: 400"
+          >
+            {{ look.name }}
+            <v-spacer></v-spacer>
+            <span>{{ look.price }}</span>
+          </v-card-title>
+          <v-divider></v-divider>
+          <v-item-group style="display: flex; justify-content: center">
+            <v-item v-for="(lab, i) in look.label" :key="i" class="mx-2 my-4">
+              <v-img
+                :src="require('@/assets/icons/' + lab + '.png')"
+                alt="bio"
+                max-width="30"
+              ></v-img>
+            </v-item>
+          </v-item-group>
+        </v-card>
+        <v-dialog
+          v-model="dialog"
+          max-width="1000"
+          hide-overlay
+          :retain-focus="false"
+          persistent
+        >
+          <v-card
+            max-width="1000"
+            style="font-family: 'Josefin Sans', sans-serif; font-weight: 400"
+          >
+            <v-card-actions>
+              <v-card-title> Look </v-card-title>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" icon fab @click="dialog = false">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <v-row>
+              <v-col cols="12" md="4" sm="12" xs="12">
+                <v-img
+                  style="margin: 0 30px 30px 30px"
+                  max-width="400px"
+                  :src="require(`@/assets/${img}`)"
+                  alt="look"
+                ></v-img>
+              </v-col>
+              <v-col cols="12" md="7" sm="12" xs="12">
+                <item-card :img="img" :look="look" />
+                <v-spacer></v-spacer>
+              </v-col>
+            </v-row>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                style="margin: 20px 10px 20px 0"
+                large
+                outlined
+                color="primary"
+              >
+                Ajouter aux favoris
+              </v-btn>
+              <v-btn
+                style="margin: 20px 10px 20px 0"
+                large
+                outlined
+                color="primary"
+              >
+                Ajouter au panier
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-	import ItemCard from "./ItemCard.vue";
-	export default {
-		name: "LookCard",
-		data() {
-			return {
-				dialog: false,
-				looks: [
-					{
-						name: "Look 1",
-						img: "",
-						price: "36 €",
-						label: ["bio", "recycle"],
-					},
-					{
-						name: "Look 2",
-						img: "",
-						price: "36 €",
-						label: [
-							"bio",
-							"europe",
-							"vegan",
-						],
-					},
-					{
-						name: "Look 3",
-						img: "",
-						price: "36 €",
-						label: ["bio"],
-					},
-					{
-						name: "Look 4",
-						img: "",
-						price: "36 €",
-						label: ["bio", "recycle"],
-					},
-					{
-						name: "Look 5",
-						img: "",
-						price: "36 €",
-						label: ["bio", "recycle"],
-					},
-					{
-						name: "Look 6",
-						img: "",
-						price: "36 €",
-						label: [
-							"bio",
-							"europe",
-							"vegan",
-						],
-					},
-					{
-						name: "Look 7",
-						img: "",
-						price: "36 €",
-						label: ["bio"],
-					},
-					{
-						name: "Look 8",
-						img: "",
-						price: "36 €",
-						label: ["bio", "recycle"],
-					},
-				],
-			};
-		},
-		computed: {
-			cat() {
-				return this.$route.params.type.toLowerCase();
-			},
-			sty() {
-				return this.$route.params.style.toLowerCase();
-			},
-			img() {
-				return `${this.cat}/${this.sty}.png`;
-			},
-		},
-		components: {
-			ItemCard,
-		},
-	};
+import ItemCard from "./ItemCard.vue";
+export default {
+  name: "LookCard",
+  data() {
+    return {
+      dialog: false,
+      looks: [
+        {
+          name: "Look 1",
+          img: "",
+          price: "36 €",
+          label: ["bio", "recycle"],
+          items: [
+            {
+              name: "Kimono coloré",
+              price: "14€",
+              img: "kimono",
+            },
+            {
+              name: "T-shirt gris",
+              price: "5€",
+              img: "tshirt",
+            },
+            {
+              name: "Jean denim",
+              price: "11€",
+              img: "jean",
+            },
+            {
+              name: "Lunettes de soleil",
+              price: "6€",
+              img: "lunettes",
+            },
+          ],
+        },
+        {
+          name: "Look 2",
+          img: "",
+          price: "36 €",
+          label: ["bio", "europe", "vegan"],
+          items: [
+            {
+              name: "Kimono coloré",
+              price: "14€",
+              img: "kimono",
+            },
+            {
+              name: "T-shirt gris",
+              price: "5€",
+              img: "tshirt",
+            },
+            {
+              name: "Jean denim",
+              price: "11€",
+              img: "jean",
+            },
+            {
+              name: "Lunettes de soleil",
+              price: "6€",
+              img: "lunettes",
+            },
+          ],
+        },
+        {
+          name: "Look 3",
+          img: "",
+          price: "36 €",
+          label: ["bio"],
+          items: [
+            {
+              name: "Kimono coloré",
+              price: "14€",
+              img: "kimono",
+            },
+            {
+              name: "T-shirt gris",
+              price: "5€",
+              img: "tshirt",
+            },
+            {
+              name: "Jean denim",
+              price: "11€",
+              img: "jean",
+            },
+            {
+              name: "Lunettes de soleil",
+              price: "6€",
+              img: "lunettes",
+            },
+          ],
+        },
+        {
+          name: "Look 4",
+          img: "",
+          price: "36 €",
+          label: ["bio", "recycle"],
+          items: [
+            {
+              name: "Kimono coloré",
+              price: "14€",
+              img: "kimono",
+            },
+            {
+              name: "T-shirt gris",
+              price: "5€",
+              img: "tshirt",
+            },
+            {
+              name: "Jean denim",
+              price: "11€",
+              img: "jean",
+            },
+            {
+              name: "Lunettes de soleil",
+              price: "6€",
+              img: "lunettes",
+            },
+          ],
+        },
+        {
+          name: "Look 5",
+          img: "",
+          price: "36 €",
+          label: ["bio", "recycle"],
+          items: [
+            {
+              name: "Kimono coloré",
+              price: "14€",
+              img: "kimono",
+            },
+            {
+              name: "T-shirt gris",
+              price: "5€",
+              img: "tshirt",
+            },
+            {
+              name: "Jean denim",
+              price: "11€",
+              img: "jean",
+            },
+            {
+              name: "Lunettes de soleil",
+              price: "6€",
+              img: "lunettes",
+            },
+          ],
+        },
+        {
+          name: "Look 6",
+          img: "",
+          price: "36 €",
+          label: ["bio", "europe", "vegan"],
+          items: [
+            {
+              name: "Kimono coloré",
+              price: "14€",
+              img: "kimono",
+            },
+            {
+              name: "T-shirt gris",
+              price: "5€",
+              img: "tshirt",
+            },
+            {
+              name: "Jean denim",
+              price: "11€",
+              img: "jean",
+            },
+            {
+              name: "Lunettes de soleil",
+              price: "6€",
+              img: "lunettes",
+            },
+          ],
+        },
+        {
+          name: "Look 7",
+          img: "",
+          price: "36 €",
+          label: ["bio"],
+          items: [
+            {
+              name: "Kimono coloré",
+              price: "14€",
+              img: "kimono",
+            },
+            {
+              name: "T-shirt gris",
+              price: "5€",
+              img: "tshirt",
+            },
+            {
+              name: "Jean denim",
+              price: "11€",
+              img: "jean",
+            },
+            {
+              name: "Lunettes de soleil",
+              price: "6€",
+              img: "lunettes",
+            },
+          ],
+        },
+        {
+          name: "Look 8",
+          img: "",
+          price: "36 €",
+          label: ["bio", "recycle"],
+          items: [
+            {
+              name: "Kimono coloré",
+              price: "14€",
+              img: "kimono",
+            },
+            {
+              name: "T-shirt gris",
+              price: "5€",
+              img: "tshirt",
+            },
+            {
+              name: "Jean denim",
+              price: "11€",
+              img: "jean",
+            },
+            {
+              name: "Lunettes de soleil",
+              price: "6€",
+              img: "lunettes",
+            },
+          ],
+        },
+      ],
+    };
+  },
+  computed: {
+    cat() {
+      return this.$route.params.type.toLowerCase();
+    },
+    sty() {
+      return this.$route.params.style.toLowerCase();
+    },
+    img() {
+      return `${this.cat}/${this.sty}.png`;
+    },
+  },
+  components: {
+    ItemCard,
+  },
+};
 </script>
 
 <style scoped></style>
